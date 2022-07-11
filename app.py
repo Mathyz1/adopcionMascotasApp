b5c20ac60de31dbd183b9d2c0f261f29884aa2e0
Mathyz1 <49544505+Mathyz1@users.noreply.github.com>
2022-07-11 01:57:37 -0300

    agregando heroku arreglado uri

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 # configuro la base de datos, con el nombre el usuario y la clave
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flaskmysql'
 #                                               user:clave@localhost/nombreBaseDatos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wrilizvyifocoo:81c42b13a206839b3e7f726abd32997d9fbc9b27d720f8260e038a9841a6a876@ec2-3-223-169-166.compute-1.amazonaws.com:5432/dd00723lmf15bj'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wrilizvyifocoo:81c42b13a206839b3e7f726abd32997d9fbc9b27d720f8260e038a9841a6a876@ec2-3-223-169-166.compute-1.amazonaws.com:5432/dd00723lmf15bj'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -22,4 +22,4 @@ from controladores.animal_controlador import *
 
 # programa principal
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
