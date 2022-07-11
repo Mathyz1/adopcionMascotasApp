a655db337bcce9e449c7ea942d4e2ca34991a022
Mathyz1 <49544505+Mathyz1@users.noreply.github.com>
2022-07-11 02:03:23 -0300

    agregando heroku arreglado uri

--- app.py
+++ app.py
@@ -5,13 +5,23 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+import os
+import re
+
 app = Flask(__name__)
 CORS(app)
 
 # configuro la base de datos, con el nombre el usuario y la clave
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flaskmysql'
 #                                               user:clave@localhost/nombreBaseDatos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wrilizvyifocoo:81c42b13a206839b3e7f726abd32997d9fbc9b27d720f8260e038a9841a6a876@ec2-3-223-169-166.compute-1.amazonaws.com:5432/dd00723lmf15bj'
+
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+# 'postgres://wrilizvyifocoo:81c42b13a206839b3e7f726abd32997d9fbc9b27d720f8260e038a9841a6a876@ec2-3-223-169-166.compute-1.amazonaws.com:5432/dd00723lmf15bj'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
